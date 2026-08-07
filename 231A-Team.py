8a2877c89e377b6198ce38b5f62ae92755571d1e
Masrur <masrurahamedmasrur1234@gmail.com>
2026-08-08 01:34:18 +0600

    3

--- 231A-Team.py
+++ 231A-Team.py
@@ -1,32 +1,7 @@
-import sys
- 
-def solve():
-    # Read all input from standard input
-    input = sys.stdin.read
-    data = input().split()
-    
-    if not data:
-        return
- 
-    # The first element is the number of problems
-    n = int(data[0])
-    
-    solved_count = 0
-    idx = 1
-    
-    # Iterate through each problem's votes
-    for _ in range(n):
-        petya = int(data[idx])
-        vasya = int(data[idx+1])
-        tonya = int(data[idx+2])
-        
-        # If the sum of confident friends is >= 2, increment the count
-        if petya + vasya + tonya >= 2:
-            solved_count += 1
-            
-        idx += 3
-        
-    print(solved_count)
- 
-if __name__ == '__main__':
-    solve()
+n = int(input())
+ans = 0
+for i in range(n):
+    a, b, c = map(int, input().split())
+    if a + b + c >= 2:
+        ans += 1
+print(ans)
