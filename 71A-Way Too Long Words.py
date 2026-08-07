be1aca6122b216083feade2d5738c3cac71735ae
Masrur <masrurahamedmasrur1234@gmail.com>
2026-08-08 01:21:29 +0600

    2

--- 71A-Way Too Long Words.py
+++ 71A-Way Too Long Words.py
@@ -1,21 +1,3 @@
-def abbreviate_words():
-    # Read the number of words
-    try:
-        n = int(input())
-    except EOFError:
-        return
- 
-    for _ in range(n):
-        word = input().strip()
-        
-        # Check if the word length is greater than 10
-        if len(word) > 10:
-            # Create abbreviation: first char + length of middle part + last char
-            abbreviation = f"{word[0]}{len(word) - 2}{word[-1]}"
-            print(abbreviation)
-        else:
-            # Print the original word if it's not "too long"
-            print(word)
- 
-if __name__ == "__main__":
-    abbreviate_words()
+for _ in range(int(input())):
+    s = input()
+    print(s if len(s) <= 10 else s[0] + str(len(s)-2) + s[-1])
