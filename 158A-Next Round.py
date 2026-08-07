6ecd92815ef9c8662dd5272d02375361e31b495e
Masrur <masrurahamedmasrur1234@gmail.com>
2026-08-08 01:48:29 +0600

    5

--- 158A-Next Round.py
+++ 158A-Next Round.py
@@ -1,27 +1,3 @@
-import sys
-
-def solve():
-    # Read n and k
-    n, k = map(int, sys.stdin.readline().split())
-    
-    # Read the scores list
-    scores = list(map(int, sys.stdin.readline().split()))
-    
-    # Identify the threshold score (k-th place finisher)
-    # Using k-1 because Python lists are 0-indexed
-    threshold_score = scores[k - 1]
-    
-    count = 0
-    for score in scores:
-        # A participant advances if they meet or beat the threshold AND have a positive score
-        if score >= threshold_score and score > 0:
-            count += 1
-        else:
-            # Since the array is sorted in descending order, if this participant 
-            # doesn't qualify, none of the remaining ones will either.
-            break
-            
-    print(count)
-
-if __name__ == '__main__':
-    solve()
+n, k = map(int, input().split())
+a = list(map(int, input().split()))
+print(sum(x >= a[k-1] and x > 0 for x in a))
