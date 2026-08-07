adf4d5e980854da6db1bb069169a5d392464b33a
Masrur <masrurahamedmasrur1234@gmail.com>
2026-08-08 01:40:04 +0600

    4

--- 282A-Bit++.py
+++ 282A-Bit++.py
@@ -1,24 +1,8 @@
-import sys
- 
-def solve():
-    # Read all inputs from standard input
-    input = sys.stdin.read().split()
-    if not input:
-        return
-    
-    n = int(input[0])
-    statements = input[1:n+1]
-    
-    x = 0
-    for statement in statements:
-        # If '+' is in the statement (e.g., "++X" or "X++"), increment x
-        if '+' in statement:
-            x += 1
-        # Otherwise, it must be '-' (e.g., "--X" or "X--"), decrement x
-        else:
-            x -= 1
-            
-    print(x)
- 
-if __name__ == '__main__':
-    solve()
+x=0
+for _ in range(int(input())):
+    s = input()
+    if '+' in s:
+        x+=1
+    else:
+        x-=1
+print(x)        
